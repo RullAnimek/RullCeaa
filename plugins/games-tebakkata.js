5c616a6730afe376eee51b3f8c507a57696072b2
RullAnimek <115926002+RullAnimek@users.noreply.github.com>
2022-11-06 19:32:38 +0700

    Update games-tebakkata.js

--- plugins/games-tebakkata.js
+++ plugins/games-tebakkata.js
@@ -1,7 +1,7 @@
 import { tebakkata } from '@bochilteam/scraper'
 
 let timeout = 120000
-let poin = 4999
+let poin = 1000
 let handler = async (m, { conn, usedPrefix }) => {
     conn.tebakkata = conn.tebakkata ? conn.tebakkata : {}
     let id = m.chat
