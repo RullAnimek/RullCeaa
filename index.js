fea5c44ad69f38a77a5d93f52fed5f1ada3022c0
RullAnimek <115926002+RullAnimek@users.noreply.github.com>
2022-11-07 05:11:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Rull Ciaaa', {
+say('Rull Ceaaa', {
   font: 'chrome',
   align: 'center',
   colors: ['red', 'magenta']
