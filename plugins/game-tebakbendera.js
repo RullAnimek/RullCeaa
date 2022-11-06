7f538b6e943c285b6540b348a77b92507f6a5367
RullAnimek <115926002+RullAnimek@users.noreply.github.com>
2022-11-06 19:34:56 +0700

    Update game-tebakbendera.js

--- plugins/game-tebakbendera.js
+++ plugins/game-tebakbendera.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 let timeout = 120000
-let poin = 4999
+let poin = 1000
 let handler = async (m, { conn, usedPrefix }) => {
     conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
     let id = m.chat
