46f6b2bf650c76c1f0e39c9b49cb7e814b7f0cc8
RullAnimek <115926002+RullAnimek@users.noreply.github.com>
2022-11-07 15:29:51 +0700

    Update menfess.js

--- plugins/menfess.js
+++ plugins/menfess.js
@@ -16,14 +16,14 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if ((!jid || !name || !pesan)) throw `*Cara penggunaan :*\n\n${usedPrefix + command} nomor|nama pengirim|pesan\n\n*Note:* nama pengirim boleh nama samaran atau anonymous.\n\n*Contoh:* ${usedPrefix + command} ${m.sender.split`@`[0]}|Anonymous|Halo.`;
     jid = jid.replace(/[^0-9]/g, '') + '@s.whatsapp.net';
     let data = (await conn.onWhatsApp(jid))[0] || {};
-    if (!data.exists) throw 'Nomer tidak terdaftar di whatsapp.';
-    if (jid == m.sender) throw 'tidak bisa mengirim pesan menfess ke diri sendiri.'
+    if (!data.exists) throw '❔ Nomer tidak terdaftar di whatsapp.';
+    if (jid == m.sender) throw '🚫 tidak bisa mengirim pesan menfess ke diri sendiri.'
     let mf = Object.values(conn.menfess).find(mf => mf.status === true)
     if (mf) return !0
     try {
     	let id = + new Date
         let txt = `Hai @${data.jid.split('@')[0]} 👋🏻, kamu menerima pesan Menfess nih.\n\nDari: *${name}*\nPesan: \n${pesan}\n\n📮 Note: Bot hanya menyampaikan pesan dari pengirim`.trim();
-        await conn.sendButton(data.jid, txt, wm, ['Balas Pesan', '.balasmenfess'], null)
+        await conn.sendButton(data.jid, txt, wm, ['Menu Bot', 'menu'], null)
         .then(() => {
             m.reply('Berhasil mengirim pesan menfess.')
             conn.menfess[id] = {
@@ -42,7 +42,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     }
 }
 handler.tags = ['Menfess']
-handler.help = ['menfess', 'mfs'].map(v => v + ' <nomor|nama pengirim|pesan>')
+handler.help = ['menfess', 'mfs'].map(v => v + ' nomor|nama pengirim|pesan')
 handler.command = /^(mfs|menfess|menfes)$/i
 handler.private = true
 handler.limit = true
