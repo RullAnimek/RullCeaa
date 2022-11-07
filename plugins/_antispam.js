63aa622d971c29a059ab4f583376f279ebbda1cb
RullAnimek <115926002+RullAnimek@users.noreply.github.com>
2022-11-07 12:12:14 +0700

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -4,10 +4,10 @@ export async function all(m) {
     this.spam = this.spam ? this.spam : {}
     if (m.sender in this.spam) {
         this.spam[m.sender].count++
-        if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 5) {
-            if (this.spam[m.sender].count > 5) {
+        if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 20) {
+            if (this.spam[m.sender].count > 20) {
                 global.db.data.users[m.sender].banned = true
-                m.reply('*📌 Kamu di banned karena spam*\n\n*💬Laporkan masalah ini ke owner')
+                m.reply('*📌 Kamu di banned karena spam*\n\n*Laporkan masalah ini ke owner')
             }
             this.spam[m.sender].count = 0
             this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
