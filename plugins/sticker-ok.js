3f48b464b1ff383a50f2fe0c298c93a29aa0f3c2
RullAnimek <115926002+RullAnimek@users.noreply.github.com>
2022-11-08 13:00:44 +0700

    Update sticker-ok.js

--- plugins/sticker-ok.js
+++ plugins/sticker-ok.js
@@ -17,5 +17,5 @@ function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())]
 }
 let stikerhuuu = [
- "https://telegra.ph/file/f61a7c96f3f86b6c5214f.png",
+ "https://telegra.ph/file/93f8d045847ebec3732cd.png",
 ]
