240b7ee26242b314986cc8d906db26a5ceb5dfe7
RullAnimek <115926002+RullAnimek@users.noreply.github.com>
2022-11-09 04:25:06 +0700

    Update fun-apakah.js

--- plugins/fun-apakah.js
+++ plugins/fun-apakah.js
@@ -1,5 +1,6 @@
 let handler = async (m) => m.reply(`
 *Pertanyaan:* ${m.text}
+
 *Jawaban:* ${['Ya', 'Mungkin iya', 'Mungkin', 'Mungkin tidak', 'Tidak', 'Tidak mungkin'].getRandom()}
   `.trim(), null, m.mentionedJid ? {
   mentions: m.mentionedJid
@@ -9,5 +10,6 @@ handler.help = ['apakah <teks>?']
 handler.tags = ['kerang', 'fun']
 handler.customPrefix = /(\?$)/
 handler.command = /^apakah$/i
+handler.limit = true
 
 export default handler
