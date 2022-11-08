e3e6fb37ce56c4aab50e21a9757c98613ea4529e
RullAnimek <115926002+RullAnimek@users.noreply.github.com>
2022-11-08 16:29:24 +0700

    Update fun-dadu.js

--- plugins/fun-dadu.js
+++ plugins/fun-dadu.js
@@ -1,11 +1,11 @@
 /* CREDITS TO https://github.com/FG98F */
 const dir = [
-  'https://tinyurl.com/ygms8wvy',
-  'https://tinyurl.com/yhdyhnap',
-  'https://tinyurl.com/yfwjbou7',
-  'https://tinyurl.com/yh3e3ogt',
-  'https://tinyurl.com/yfmhpvxs',
-  'https://tinyurl.com/ygpxka9q'
+  'https://telegra.ph/file/03fdac681d792e5cfd353.mp4',
+  'https://telegra.ph/file/ae9d106bbacfea811ab85.mp4',
+  'https://telegra.ph/file/852f187ae2d669671e115.mp4',
+  'https://telegra.ph/file/5f9e5327fef3ed4962adf.mp4',
+  'https://telegra.ph/file/acf956f34f3c159c81004.mp4',
+  'https://telegra.ph/file/b7bfcea83d05266eddd53.mp4'
 ];
 let handler = async (m, { conn }) => {
 conn.sendFile(m.chat, dir[Math.floor(Math.random() * dir.length)], 'dadu.webp', '', m)
