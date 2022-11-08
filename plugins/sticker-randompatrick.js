b1d17a9803ecffbd19ff4e30a0af3d11acedddd3
RullAnimek <115926002+RullAnimek@users.noreply.github.com>
2022-11-09 04:24:09 +0700

    Update sticker-randompatrick.js

--- plugins/sticker-randompatrick.js
+++ plugins/sticker-randompatrick.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
 	let res = 'https://api.lolhuman.xyz/api/sticker/patrick?apikey=64333e1c050ced435defe154'
-	conn.sendFile(m.chat, res, 'xynz.webp', `ꜱᴜᴅᴀʜ ᴊᴀᴅɪ`, m, false)
+	conn.sendFile(m.chat, res, 'slimebot.webp', `Done`, m, false)
 }
 handler.command = /^(spatrick)$/i
 handler.tags = ['premium', 'sticker']
