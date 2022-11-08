fda5fc61a17d25969e15f3441fa4cb9e855f8c90
RullAnimek <115926002+RullAnimek@users.noreply.github.com>
2022-11-08 16:58:48 +0700

    Update sticker-ttp.js

--- plugins/sticker-ttp.js
+++ plugins/sticker-ttp.js
@@ -1,20 +1,14 @@
 import { sticker } from '../lib/sticker.js'
-import fetch from 'node-fetch'
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `✳️ _Kirim teks_\n\n📌Contoh *${usedPrefix + command}* Slime Bot`  
-  try {
-    let url = await fetch(global.API('https://salism3api.pythonanywhere.com', '/text2img/', { text } ))
-    let res = await url.json()
-    let stick = res.image
-    let stiker = await sticker(null, stick, global.packname, global.author)
-    if (stiker) return await conn.sendFile(m.chat, stiker, '', '', m, null, rpl)
-  } catch (e) {
-    m.reply('Error')
-    throw false
-  }
+
+let handler = async (m, { conn, text }) => {
+    let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
+    let stiker = await sticker(null, global.API('xteam', '/ttp', { file: '', text: teks }), global.packname, global.author)
+    if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+    throw stiker.toString()
 }
-handler.help = ['ttp (text)']
+handler.help = ['ttp <teks>']
 handler.tags = ['sticker']
-handler.command = ['ttp']
+handler.command = /^ttp$/i
 handler.limit = true
+
 export default handler
